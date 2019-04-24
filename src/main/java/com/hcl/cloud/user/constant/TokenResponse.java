5bd029619f262f8201dcc1f98b6078862daf9d37
dineshkumar.sharma <dineshkumar.sharma@LP-5CD84714JM.HCLT.CORP.HCL.IN>
2019-04-24 20:45:18 +0530

    Token response

---
+++ src/main/java/com/hcl/cloud/user/constant/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.hcl.cloud.user.constant;
+
+public class TokenResponse {
+private String userId;
+
+public String getUserId() {
+	return userId;
+}
+
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+
+}
